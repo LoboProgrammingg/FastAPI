e59248b93364b69868aaecd3403f3ea7f63a89c2
loboprogrammingg <matheusloboo2001@gmail.com>
2025-06-22 18:03:21 -0400

    Configurando Banco de Dados

--- src/learning_fastapi/app.py
+++ src/learning_fastapi/app.py
@@ -1,13 +1,16 @@
 from http import HTTPStatus
 
 from fastapi import FastAPI, HTTPException
+from sqlalchemy import select, or_
+from .database import get_session
+from sqlalchemy.orm import Session
 
+from .models import User
 from .schemas import Message, UserDB, UserList, UserPublic, UserSchema
 
 app = FastAPI()
 
 database = []
-
 
 @app.get('/', status_code=HTTPStatus.OK, response_model=Message)
 def read_root():
@@ -16,14 +19,33 @@ def read_root():
 
 @app.post('/users/', status_code=HTTPStatus.CREATED, response_model=UserPublic)
 def create_user(user: UserSchema):
-    user_with_id = UserDB(
-        id=len(database) + 1,
-        **user.model_dump(),
+    session = get_session()
+    db_user = session.scalar(
+        select(User).where(
+        or_(User.username == user.username, User.email == user.email)
+        )
     )
 
-    database.append(user_with_id)
+    if db_user:
+        if db_user.username == user.username:
+            raise HTTPException(
+                status_code=HTTPStatus.BAD_REQUEST,
+                detail='Username already exists',
+            )
+        elif db_user.email == user.email:
+            raise HTTPException(
+                status_code=HTTPStatus.BAD_REQUEST,
+                detail='Email already exists',
+            )
 
-    return user_with_id
+    db_user = User(
+        username=user.username, email=user.email, password=user.password
+    )
+    session.add(db_user)
+    session.commit()
+    session.refresh(db_user)
+
+    return db_user
 
 
 @app.get('/users/', response_model=UserList)
